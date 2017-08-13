5efafb6dce216126904087c1dc5fdaf4c5221e76
Carlos Del Aguila Rios <cdelaguilarios@gmail.com>
2017-08-13 18:34:57 -0500

    Cambios gen.

--- wp-content/themes/eah/functions.php
+++ wp-content/themes/eah/functions.php
@@ -5,7 +5,7 @@
   Theme URI:    http://englishathomeperu.com/
   Description:  Tema principal para el sitio web de English at Home Perú
   Author:       Carlos Del Aguila Rios
-  Author URI:   
+  Author URI:
   Template:     divi
   Version:      1.0.0
  */
@@ -13,3 +13,15 @@ require_once __DIR__ . '/inc/tipos_publicacion.php';
 require_once __DIR__ . '/inc/taxonomias.php';
 require_once __DIR__ . '/inc/shortcodes.php';
 require_once __DIR__ . '/inc/registro_interesado.php';
+
+function wptricks24_recaptcha_scripts() {
+  wp_deregister_script('google-recaptcha');
+  $url = 'https://www.google.com/recaptcha/api.js';
+  $url = add_query_arg(array(
+      'onload' => 'recaptchaCallback',
+      'render' => 'explicit',
+      'hl' => 'es'), $url);
+  wp_register_script('google-recaptcha', $url, array(), '2.0', true);
+}
+
+add_action('wpcf7_enqueue_scripts', 'wptricks24_recaptcha_scripts', 11);
